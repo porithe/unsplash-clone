ca7834c33b7cf6e12b77f9bfc006f7ab0da1d6e8
porithe <malkowski.buisness@gmail.com>
2019-09-20 07:55:47 +0200

    edit readme

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.scss';
+
+const Header = () => {
+
+  return (
+    <header className="header">
+      <h1 className="header__title">beauty</h1>
+      
+    </header>
+  )
+};
+
+export default Header;
