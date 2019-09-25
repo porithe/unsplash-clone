e92bf634386c5d7f304921ed43f22677ea35142b
porithe <malkowski.buisness@gmail.com>
2019-09-25 10:42:35 +0200

    added full responsive modal, added exit button to modal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -5,6 +5,7 @@ import './Modal.scss';
 const Modal = ({ isShowing, hide, imageData }) => isShowing ? ReactDOM.createPortal(
   <React.Fragment>
     <div className="modal">
+      <button className="modal__exitbutton" onClick={hide}></button>
       <img className={imageData.height > imageData.width ? "modal__img--height" : "modal__img--width"} src={imageData.urls.regular} alt={imageData.alt_description} />
     </div>
   </React.Fragment>, document.body
